b1dc6b1f854eb406da9b16cf0e8b1b18280dcd0f
Zied Guellim <guellim.z@sfeir.com>
2019-04-05 15:48:24 +0200

    init app engine

--- main.go
+++ main.go
@@ -4,11 +4,20 @@ import (
 	"fmt"
 	"github.com/ziellim/cuvi/model"
 	"net/http"
+	"github.com/rs/cors"
+	"github.com/gorilla/mux"
+	"log"
 )
 
 func main() {
-	http.HandleFunc("/", getOne)
-	http.ListenAndServe(":8080", nil)
+	c := cors.New(cors.Options{
+		AllowedOrigins: []string{"http://localhost:4200"},
+	})
+	router := mux.NewRouter()
+	router.HandleFunc("/v0/cvs/0", getOne).Methods("GET")
+	log.Fatal(http.ListenAndServe(":8080", c.Handler(router)))
+	http.ListenAndServe(":8080", c.Handler(router))
+
 }
 
 func getOne(w http.ResponseWriter, r *http.Request) {
